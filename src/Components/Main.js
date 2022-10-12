1f4bf7b5b1416f58123656bd90b3d15d2222436b
Mahender Pratap <91627250+MahiPratap@users.noreply.github.com>
2022-10-13 01:02:25 +0530

    ffirst

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -9,7 +9,7 @@ import { useEffect } from "react";
 const Main = () => {
     const [pokeData, setPokeData] = useState([]);
     const [loading, setLoading] = useState(true);
-    const [url, setUrl] = useState('https://pokeapi.co/api/v2/pokemon?limit=20')
+    const [url, setUrl] = useState('https://pokeapi.co/api/v2/pokemon?limit=20&offset=0')
     const [nextUrl, setNextUrl] = useState();
     const [prevUrl, setPrevUrl] = useState();
     const [pokeDex, setPokeDex] = useState();
@@ -32,30 +32,49 @@ const Main = () => {
             })
         })
     }
+
+    if (name) {
+        url = `https://pokeapi.co/api/v2/pokemon?${inputVal.name}`;
+      } 
+      else if (name === false) {
+        url = `https://api.tvmaze.com/search/shows?q=${inputVal.id}`;
+      }
+
     useEffect(() => {
         pokeFun();
     }, [url])
 
-    const changeInput = (e)=>{
-  
+    const changeInput = (e) => {
+
         setUrl(e.target.value)
-      }
+    }
     return (
         <div>
-            <input id="input" onChange={changeInput} />
+
             <div className="container">
-            
+                <div className="header">
+                    <input className="search-bar"
+                        placeholder="search here"
+                        typeof="text"
+                        onChange={changeInput}
+                    />
+                    <select className="selector">
+                        <option value="name">name</option>
+                        <option value="id">id</option>
+                    </select>
+                </div>
+
                 <div className="up-content">
-                
+
                     <Card pokemon={pokeData} loading={loading} infoPokemon={poke => setPokeDex(poke)} />
 
                     <div className="btn-group">
-                        { <button onClick={() => {
+                        {<button onClick={() => {
                             setPokeData([])
                             setUrl(prevUrl)
                         }}>Previous</button>}
 
-                        { <button onClick={() => {
+                        {<button onClick={() => {
                             setPokeData([])
                             setUrl(nextUrl)
                         }}>Next</button>}
